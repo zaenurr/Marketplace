bda45d6e2dccce6ce22ee2bd60ba0605c789bdb7
Gembelelitz <43411644+Gembelelitz@users.noreply.github.com>
2018-12-17 13:06:31 +0700

    Create shopping-cart.php

---
+++ view/shopping-cart.php
@@ -0,0 +1,101 @@
+
+        <link rel="stylesheet" href="<?php echo $host?>asset/stylesheet/bootstrap.css">
+        <link rel="stylesheet" href="<?php echo $host?>asset/stylesheet/uielement.css">
+        
+       
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="panel nm" style="border-bottom-width:1px;">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th width="15%">Image</th>
+                                            <th>Descriptions</th>
+                                            <th width="15%">Quantity</th>
+                                            <th width="15%">Price</th>
+                                            <th width="5%">#</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <!-- thumbnail -->
+                                                <div class="thumbnail nm">
+                                                    <!-- media -->
+                                                    <div class="media">
+                                                        <!-- indicator -->
+                                                        <div class="indicator"><span class="spinner"></span></div>
+                                                        <!--/ indicator -->
+
+                                                        <img data-toggle="unveil" src="<?php echo $host?>asset/image/shop/placeholder.jpg" data-src="<?php echo $host?>asset/image/shop/item1.jpg" alt="Photo" width="100%" />
+                                                    </div>
+                                                    <!--/ media -->
+                                                </div>
+                                                <!--/ thumbnail -->
+                                            </td>
+                                            <td>
+                                                <h4 class="mt0">505 Fit Jeans</h4>
+                                                <p class="nm text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                tempor incididunt ut labore et dolore magna aliqua.</p>
+                                            </td>
+                                            <td>
+                                                <h4 class="font-alt nm">x5</h4>
+                                            </td>
+                                            <td>
+                                                <h4 class="font-alt text-primary nm">$102.50</h4>
+                                            </td>
+                                            <td>
+                                                <h5 class="nm"><a href="#"><i class="ico-close2"></i></a></h5>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <!-- thumbnail -->
+                                                <div class="thumbnail nm">
+                                                    <!-- media -->
+                                                    <div class="media">
+                                                        <!-- indicator -->
+                                                        <div class="indicator"><span class="spinner"></span></div>
+                                                        <!--/ indicator -->
+
+                                                        <img data-toggle="unveil" src="<?php echo $host?>asset/image/shop/placeholder.jpg" data-src="<?php echo $host?>asset/image/shop/item2.jpg" alt="Photo" width="100%" />
+                                                    </div>
+                                                    <!--/ media -->
+                                                </div>
+                                                <!--/ thumbnail -->
+                                            </td>
+                                            <td>
+                                                <h4 class="mt0">505 Regular Fit</h4>
+                                                <p class="nm text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                tempor incididunt ut labore et dolore magna aliqua.</p>
+                                            </td>
+                                            <td>
+                                                <h4 class="font-alt nm">x2</h4>
+                                            </td>
+                                            <td>
+                                                <h4 class="font-alt text-primary nm">$211.50</h4>
+                                            </td>
+                                            <td>
+                                                <h5 class="nm"><a href="#"><i class="ico-close2"></i></a></h5>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                        
+        <!-- START JAVASCRIPT SECTION (Load javascripts at bottom to reduce load time) -->
+        <!-- Application and vendor script : mandatory -->
+        <script type="text/javascript" src="<?php echo $host?>asset/javascript/vendor.js"></script>
+        <script type="text/javascript" src="<?php echo $host?>asset/javascript/core.js"></script>
+        <script type="text/javascript" src="<?php echo $host?>asset/javascript/frontend/app.js"></script>
+        <!--/ Application and vendor script : mandatory -->
+
+        <!-- Plugins and page level script : optional -->
+        <script type="text/javascript" src="<?php echo $host?>asset/plugins/smoothscroll/js/smoothscroll.js"></script>
+        <!--/ Plugins and page level script : optional -->
+        <!--/ END JAVASCRIPT SECTION -->
+    </body>
+    <!--/ END Body -->
+</html>
